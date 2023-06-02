da6e29d1785015381b1359b058f1f30a53399e5b
Martin Schlegel <m.schlegel81@googlemail.com>
2023-06-02 22:36:23 +0200

    Compiler update

--- build32bit.bat
+++ build32bit.bat
@@ -1,3 +1,3 @@
 ..\lazarus32\lazbuild.exe sudoku3Project.lpi --bm=Default
-..\lazarus32\fpc\3.0.4\bin\i386-win32\delp.exe . common
+..\lazarus32\fpc\3.2.0\bin\i386-win32\delp.exe . common
 del sudoku3Project.res
