6a365158ec223f14a3b5c20e2ac202dd5023c17d
Martin Schlegel <schlegel@itestra.com>
2023-06-13 09:06:10 +0200

    Updated build script

--- build32bit.bat
+++ build32bit.bat
@@ -1,3 +1,4 @@
 ..\lazarus32\lazbuild.exe sudoku3Project.lpi --bm=Default
-..\lazarus32\fpc\3.2.0\bin\i386-win32\delp.exe . common
-del sudoku3Project.res
+..\lazarus32\fpc\3.2.2\bin\i386-win32\delp.exe -r . 
+upx.exe --best --ultra-brute -o sudoku_upx.exe sudoku.exe
+move /Y sudoku_upx.exe sudoku.exe
