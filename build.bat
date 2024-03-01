94d842f8e67e7c0372b595339062fae709dc225f
Martin Schlegel <schlegel@itestra.com>
2024-03-01 10:18:46 +0100

    Removed UPX from build (trojan warning by Windows Defender)

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 ..\..\lazarus64\lazbuild.exe sudoku3Project.lpi --bm=Default
-..\..\lazarus64\fpc\3.2.2\bin\x86_64-win64\delp.exe -r . 
-upx.exe --best --ultra-brute sudoku.exe
+..\..\lazarus64\fpc\3.2.2\bin\x86_64-win64\delp.exe -r .
